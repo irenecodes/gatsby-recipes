595404d2479e03b370744b731be2353aea433670
Irene <irenesis123@gmail.com>
2021-12-05 03:11:14 -0500

    playing around with styled

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,35 @@
 import React from "react"
 import Layout from "../components/Layout.component"
 import * as aboutStyles from "../examples/about.module.css"
+import styled from "styled-components"
 const about = () => {
   return (
     <Layout>
       <div className={aboutStyles.page}>
         <p>About heading</p>
       </div>
+      <Wrapper>
+        <h1>
+          testing styled <span>test</span>
+        </h1>
+        <p className="para">hello</p>
+      </Wrapper>
       About Page
     </Layout>
   )
 }
+
+const Wrapper = styled.section`
+  color: steelblue;
+  h1 {
+    color: gold;
+    span {
+      color: red;
+    }
+  }
+  .para {
+    color: gray;
+  }
+`
 
 export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/Layout.component"
 
 import { page, text } from "../examples/home.module.css"
-
+import { ExampleButton } from "../examples/button.styled"
 export default function Home() {
   return (
     <Layout>
@@ -10,6 +10,7 @@ export default function Home() {
         <h1>Home heading</h1>
         <p>i'll be blue text from home module</p>
         <p className={text}>caps coming from home module</p>
+        <ExampleButton>Click me</ExampleButton>
       </div>
       My awesome root home page
     </Layout>
