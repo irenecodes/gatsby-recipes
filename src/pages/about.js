ec9945f22029a6bb6584db281d028c43ab76b755
Irene <irenesis123@gmail.com>
2021-12-05 02:31:06 -0500

    css module sample added

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,14 @@ import React from "react"
 import Layout from "../components/Layout.component"
 
 const about = () => {
-  return <Layout>About Page</Layout>
+  return (
+    <Layout>
+      <div className="about-heading">
+        <h1>About heading</h1>
+      </div>
+      About Page
+    </Layout>
+  )
 }
 
 export default about
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,17 @@
 import React from "react"
-
 import Layout from "../components/Layout.component"
 
+import { page, text } from "../examples/home.module.css"
+
 export default function Home() {
-  return <Layout>My awesome root home page</Layout>
+  return (
+    <Layout>
+      <div className={page}>
+        <h1>Home heading</h1>
+        <p>i'll be blue text from home module</p>
+        <p className={text}>caps coming from home module</p>
+      </div>
+      My awesome root home page
+    </Layout>
+  )
 }
